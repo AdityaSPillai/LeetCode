589bd95e997634be3af8d3e0acce6ebadfe27c74
Aditya S <119068813+AdityaSPillai@users.noreply.github.com>
2026-01-09 10:14:46 +0530

    15 Solved

---
+++ 2026/09_01_26/15.py
@@ -0,0 +1,23 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res=[]
+        for i in range(len(nums)):
+            l,r=i+1,len(nums)-1
+            if nums[i]==nums[i-1] and i!=0:
+                continue
+            while l<r:
+                t=nums[i]+nums[l]+nums[r]
+                if t==0:
+                    res.append([nums[i],nums[l],nums[r]])
+                    while l<r and nums[l]==nums[l+1]:
+                        l+=1
+                    while l<r and nums[r]==nums[r-1]:
+                        r-=1
+                    l+=1
+                    r-=1
+                elif t>0:
+                    r-=1
+                else:
+                    l+=1
+        return res
