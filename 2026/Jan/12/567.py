e1ef7c31e90d3296a8ee420843c686d41293c63e
Aditya S <119068813+AdityaSPillai@users.noreply.github.com>
2026-01-12 23:19:19 +0530

    Solved 567

---
+++ 2026/Jan/12/567.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1)>len(s2):
+            return False
+        s1s,s2s=[0]*26,[0]*26
+        match=0
+        for i in range(len(s1)):
+            s1s[ord(s1[i])-ord('a')]+=1
+            s2s[ord(s2[i])-ord('a')]+=1
+        for i in range(26):
+            if s1s[i]==s2s[i]:
+                match+=1
+        for i in range(1,len(s2)-len(s1)+1):
+            if match==26:
+                return True
+
+            if s2s[ord(s2[i-1])-ord('a')]==s1s[ord(s2[i-1])-ord('a')]:
+                match-=1
+            elif s2s[ord(s2[i-1])-ord('a')]>s1s[ord(s2[i-1])-ord('a')]:
+                match+=1
+            s2s[ord(s2[i-1])-ord('a')]-=1
+
+            if s2s[ord(s2[i+len(s1)-1])-ord('a')]==s1s[ord(s2[i+len(s1)-1])-ord('a')]:
+                match-=1
+            elif s2s[ord(s2[i+len(s1)-1])-ord('a')]<s1s[ord(s2[i+len(s1)-1])-ord('a')]:
+                match+=1
+            s2s[ord(s2[i+len(s1)-1])-ord('a')]+=1
+        return match==26
