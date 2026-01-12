d55a4e074b2def90d1d339e987fd29f603872054
Aditya S <119068813+AdityaSPillai@users.noreply.github.com>
2026-01-12 21:36:31 +0530

    Update 84.py

--- 2026/Jan/12/84.py
+++ 2026/Jan/12/84.py
@@ -24,3 +24,27 @@ class Solution:
             #print(popped,area,(popped[1]*(len(heights)-popped[0])),"Calculation: ",popped[1],len(heights),popped[0])
             area=max(area,(popped[1]*((len(heights))-popped[0])))
         return area
+
+#Leetcode Submission
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack=[[0,heights[0]]]
+        area=0
+        for i in range(len(heights)):
+            if heights[i]>stack[-1][1]:
+                stack.append([i,heights[i]])
+            elif heights[i]==stack[-1][1]:
+                stack.append([stack[-1][0],heights[i]])
+            else:
+                popped=stack.pop()
+                area=max(area,(popped[1]*(i-popped[0])))
+                index=popped[0]
+                while stack and heights[i]<stack[-1][1]:
+                    popped=stack.pop()
+                    area=max(area,(popped[1]*(i-popped[0])))
+                    index=popped[0]
+                stack.append([index,heights[i]])
+        while stack:
+            popped=stack.pop()
+            area=max(area,(popped[1]*(len(heights)-popped[0])))
+        return area
