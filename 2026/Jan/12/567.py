4d68cd10d717157651cad8500285ac2e4945b3e9
Aditya S <119068813+AdityaSPillai@users.noreply.github.com>
2026-01-13 09:14:38 +0530

    Correct Solution for 567.py

--- 2026/Jan/12/567.py
+++ 2026/Jan/12/567.py
@@ -26,3 +26,42 @@ class Solution:
                 match+=1
             s2s[ord(s2[i+len(s1)-1])-ord('a')]+=1
         return match==26
+
+#Correct Solution
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1)>len(s2):
+            return False
+        s1s,s2s=[0]*26,[0]*26
+        matches=0
+        for i in range(len(s1)):
+            s1s[ord(s1[i])-ord('a')]+=1
+            s2s[ord(s2[i])-ord('a')]+=1
+        for i in range(26):
+            if s1s[i]==s2s[i]:
+                matches+=1
+        
+        for i in range(1,len(s2)-len(s1)+1):
+            if matches==26:
+                return True
+                
+            if s2s[ord(s2[i-1])-ord('a')]==s1s[ord(s2[i-1])-ord('a')]:
+                matches-=1
+                s2s[ord(s2[i-1])-ord('a')]-=1
+            elif s2s[ord(s2[i-1])-ord('a')]>s1s[ord(s2[i-1])-ord('a')]:
+                s2s[ord(s2[i-1])-ord('a')]-=1
+                if s2s[ord(s2[i-1])-ord('a')]==s1s[ord(s2[i-1])-ord('a')]:
+                    matches+=1
+            else:
+                s2s[ord(s2[i-1])-ord('a')]-=1
+            if s2s[ord(s2[i+len(s1)-1])-ord('a')]==s1s[ord(s2[i+len(s1)-1])-ord('a')]:
+                s2s[ord(s2[i+len(s1)-1])-ord('a')]+=1
+                matches-=1
+            elif s2s[ord(s2[i+len(s1)-1])-ord('a')]<s1s[ord(s2[i+len(s1)-1])-ord('a')]:
+                s2s[ord(s2[i+len(s1)-1])-ord('a')]+=1
+                if s2s[ord(s2[i+len(s1)-1])-ord('a')]==s1s[ord(s2[i+len(s1)-1])-ord('a')]:
+                    matches+=1
+            else:
+                s2s[ord(s2[i+len(s1)-1])-ord('a')]+=1
+
+        return matches==26
