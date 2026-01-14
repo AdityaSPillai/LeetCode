97a399494601b0073ff2c45138127e060b33645c
Aditya S <119068813+AdityaSPillai@users.noreply.github.com>
2026-01-14 18:52:47 +0530

    Update 74.py

--- 2026/Jan/14/74.py
+++ 2026/Jan/14/74.py
@@ -15,3 +15,21 @@ class Solution:
                 mid=(mid1*len(matrix[0]))+mid2
                 l=mid+1
         return False
+
+#Leetcode Submission
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        c=len(matrix[0])
+        l,r=0,(len(matrix)*c)-1
+        while l<=r:
+            mid=(l+(r-l)//2)
+            mid1,mid2=mid//c,mid%c
+            if matrix[mid1][mid2]==target:
+                return True
+            elif matrix[mid1][mid2]>target:
+                r=mid-1
+            else:
+                l=mid+1
+        return False
