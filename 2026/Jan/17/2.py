394639716265589061a7a0b7e4dbcf05947f7643
Aditya S <119068813+AdityaSPillai@users.noreply.github.com>
2026-01-17 04:19:46 +0530

    Update 2.py

--- 2026/Jan/17/2.py
+++ 2026/Jan/17/2.py
@@ -54,3 +54,54 @@ class Solution:
             newNode=ListNode(carry)
             prev.next=newNode
         return l1
+
+#LeetCode Submission
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        curr1,curr2=l1,l2
+        carry=0
+        prev=None
+        while curr1 and curr2:
+            val1,val2=curr1.val,curr2.val
+            add=val1+val2+carry
+            if add>=10:
+                carry=1 
+                add=add%10
+            else:
+                carry=0
+            curr1.val=add
+            prev=curr1
+            curr1=curr1.next
+            curr2=curr2.next
+        while curr1:
+            val1=curr1.val
+            add=val1+carry
+            if add>=10:
+                carry=1 
+                add=add%10
+            else:
+                carry=0
+            curr1.val=add
+            prev=curr1
+            curr1=curr1.next
+        while curr2:
+            val2=curr2.val
+            add=val2+carry
+            if add>=10:
+                carry=1 
+                add=add%10
+            else:
+                carry=0
+            newNode=ListNode(add)
+            prev.next=newNode
+            prev=newNode
+            curr2=curr2.next
+        if carry==1:
+            newNode=ListNode(carry)
+            prev.next=newNode
+        return l1
