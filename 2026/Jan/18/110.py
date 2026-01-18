19c4f02e6a2b1c30e4a202e768538718582b9194
Aditya S <119068813+AdityaSPillai@users.noreply.github.com>
2026-01-18 15:32:57 +0530

    Improved 110

--- 2026/Jan/18/110.py
+++ 2026/Jan/18/110.py
@@ -17,3 +17,24 @@ class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         self.DFS(root)
         return True if self.res==0 else False
+
+#Better Algorithm
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.res=0
+    def DFS(self,root):
+        if not root:
+            return 0
+        l,r=self.DFS(root.left),self.DFS(root.right)
+        if l-r not in [-1,0,1]:
+            self.res=1
+        return 1+max(l,r)
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.DFS(root)
+        return True if self.res==0 else False
