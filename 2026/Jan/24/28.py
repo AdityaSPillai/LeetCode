0cbc5899885ac15105fbcb1696d73a486ffc466b
Aditya S <119068813+AdityaSPillai@users.noreply.github.com>
2026-01-24 12:48:45 +0530

    Solved 28

---
+++ 2026/Jan/24/28.py
@@ -0,0 +1,23 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        tocheck=[]
+        lenh=len(haystack)
+        if len(needle)>lenh:
+            return -1
+        for i in range(lenh):
+            if haystack[i]==needle[0]:
+                tocheck.append(i)
+        print(tocheck)
+        for i in tocheck:
+            no=0
+            for j in range(len(needle)):
+                if i+j<lenh:
+                    if haystack[i+j]!=needle[j]:
+                        no=1
+                        break
+                else:
+                    no=1
+                    break
+            if no==0 and j>=len(needle)-1:
+                return i
+        return -1
