72a1a0bb734a8e47ca9d4c4fc39c907b1b58682c
Aditya S <119068813+AdityaSPillai@users.noreply.github.com>
2026-01-27 13:44:21 +0530

    Solved 79

---
+++ 2026/Jan/27/79.py
@@ -0,0 +1,43 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        idx=[]
+        n=len(board[0])
+        m=len(board)
+        self.found=0
+        for i in range(m):
+            for j in range(n):
+                if board[i][j]==word[0]:
+                    idx.append([i,j])
+        def backtracking(i,j,subset,indx):
+            if not subset:
+                self.found=1
+                return
+            if i+1<m and board[i+1][j]==subset[0] and [i+1,j] not in indx:
+                indx.append([i,j])
+                backtracking(i+1,j,subset[1:],indx)
+                if self.found==1:
+                    return
+                indx.pop()
+            if j+1<n and board[i][j+1]==subset[0] and [i,j+1] not in indx:
+                indx.append([i,j])
+                backtracking(i,j+1,subset[1:],indx)
+                if self.found==1:
+                    return
+                indx.pop()
+            if i-1>=0 and board[i-1][j]==subset[0] and [i-1,j] not in indx:
+                indx.append([i,j])
+                backtracking(i-1,j,subset[1:],indx)
+                if self.found==1:
+                    return
+                indx.pop()
+            if j-1>=0 and board[i][j-1]==subset[0] and [i,j-1] not in indx:
+                indx.append([i,j])
+                backtracking(i,j-1,subset[1:],indx)
+                if self.found==1:
+                    return
+                indx.pop()
+        for i,j in idx:
+            backtracking(i,j,word[1:],[[i,j]])
+            if self.found==1:
+                return True
+        return False
